60273c686b3748b86e3c047a35c0a2ddda1d3585
finntan-mhclg <finntan-mhclg>
2025-09-16 15:40:41 +0000

    fix

--- tests/_bootstrap.py
+++ tests/_bootstrap.py
@@ -7,5 +7,10 @@ spark = SparkSession.builder.getOrCreate()
 user_email = spark.sql("SELECT current_user()").collect()[0][0]
 
 repo_root = f"/Workspace/Users/{user_email}/cdp_core/src"
+
 if repo_root not in sys.path:
     sys.path.append(repo_root)
+
+sp_root = f"/Workspace/Users/{user_email}/.bundle/cdp_core/files/src"
+if sp_root not in sys.path:
+    sys.path.append(sp_root)
