33af56e2a1f4c02a9b8fac4c471dd35464a52c28
finntan-mhclg <finntan-mhclg>
2025-09-17 07:01:29 +0000

    add paths

--- tests/_bootstrap.py
+++ tests/_bootstrap.py
@@ -6,11 +6,12 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 user_email = spark.sql("SELECT current_user()").collect()[0][0]
 
-repo_root = f"/Workspace/Users/{user_email}/cdp_core/src"
+paths = [
+    f"/Workspace/Users/{user_email}/cdp_core/src",
+    f"/Workspace/Users/{user_email}/.bundle/cdp_core/files",
+    f"/Workspace/Users/{user_email}/.bundle/cdp_core/files/src"
+]
 
-if repo_root not in sys.path:
-    sys.path.append(repo_root)
-
-sp_root = f"/Workspace/Users/{user_email}/.bundle/cdp_core/files"
-if sp_root not in sys.path:
-    sys.path.append(sp_root)
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
