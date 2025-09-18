8ee915f9d801403489ec779dae336ca82055f91a
Finn Wee Lip <finn.tan@test.communities.gov.uk>
2025-09-18 08:29:27 +0100

    fix

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,13 +1,9 @@
-import sys
-sys.dont_write_bytecode = True
-
-import os
+import os, sys
+sys.dont_write_bytecode = True # skip writing compiled .pyc files in __pycache__. This is done prior to importing pytest.
 
 import pytest
-from pyspark.sql import SparkSession
 from pyspark.dbutils import DBUtils
-
-import _bootstrap
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
@@ -18,8 +14,14 @@ notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext(
 # Get the repo's root directory name.
 repo_root = os.path.dirname(os.path.dirname(notebook_path))
 
-# Prepare to run pytest from the repo.
-os.chdir(f"/Workspace/{repo_root}")
+# Define the subdirectories to ad to sys.path
+subdirs = ["tests", "src"]
+
+# Insert each path into sys.path if not already present
+for i, subdir in enumerate(subdirs):
+    path = f"/Workspace/{repo_root}/{subdir}/"
+    if path not in sys.path:
+        sys.path.insert(i, path)
 
 # Run pytest.
 retcode = pytest.main([".", "-v", "-p", "no:cacheprovider", "--disable-warnings"])
