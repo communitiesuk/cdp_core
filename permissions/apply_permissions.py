b00141c5abbe70c2af658e00a6e1c0ab97204eb0
Finn Wee Lip <finn.tan@test.communities.gov.uk>
2025-11-24 08:46:26 +0000

    update scripts

---
+++ permissions/apply_permissions.py
@@ -0,0 +1,22 @@
+import sys
+
+catalog_name = sys.argv[1]
+access_table = sys.argv[2]
+permission = sys.argv[3]
+
+df = spark.sql(
+    f"""
+        SELECT catalog_name, schema_name, table_name
+        FROM {catalog_name}.information_schema.{access_table}
+    """
+)
+
+for row in df.collect():
+    sql = (
+        f"""
+        {permission} SELECT ON `{row['catalog_name']}`.`{row['schema_name']}`.`{row['table_name']}` 
+        TO `account users`
+        """
+    )
+    print(sql)
+    spark.sql(sql)
